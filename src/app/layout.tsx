048a54e38f3636c1722f4a283b52a5502bae4ff3
Debjit-Ghosh2408 <Debjit.ghosh@provis.ai>
2026-02-17 23:55:07 +0530

    OG tag will visible

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,25 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
     title: "Smart Bookmark Manager",
     description: "Manage your bookmarks with real-time sync across devices",
+    openGraph: {
+        title: "Smart Bookmark Manager",
+        description: "Manage your bookmarks with real-time sync across devices",
+        images: [
+            {
+                url: "/Frontimage.jpeg",
+                width: 1200,
+                height: 630,
+                alt: "Smart Bookmark Manager",
+            },
+        ],
+        type: "website",
+    },
+    twitter: {
+        card: "summary_large_image",
+        title: "Smart Bookmark Manager",
+        description: "Manage your bookmarks with real-time sync across devices",
+        images: ["/Frontimage.jpeg"],
+    },
 };
 
 export default function RootLayout({
